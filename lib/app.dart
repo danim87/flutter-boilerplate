ba13daa22d50064a619f83b9709c580e6e98a339
Tobias Weiss <tobias.weiss@bytabo.de>
2021-01-10 17:01:57 +0100

    added ChangeNotifierProvider and BoilerPlateApp to app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_boilerplate_starter/providers/nav_bar_provider.dart';
+import 'package:flutter_boilerplate_starter/widgets/nav_bar/nav_bar.dart';
 import 'package:provider/provider.dart';
 
 import 'providers/app_provider.dart';
 import 'router.dart';
+import 'services/size_service.dart';
+import 'widgets/home/home.dart';
+import 'widgets/settings/settings.dart';
 
 class App extends StatelessWidget {
   @override
@@ -10,6 +15,7 @@ class App extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => AppProvider()),
+        ChangeNotifierProvider(create: (context) => NavBarProvider()),
       ],
       child: MaterialApp(
         title: 'Flutter Boilerplate Starter',
@@ -20,6 +26,27 @@ class App extends StatelessWidget {
         initialRoute: '/',
         onGenerateRoute: AppRouter.generateRoute,
       ),
+    );
+  }
+}
+
+class BoilerPlateApplication extends StatefulWidget {
+  @override
+  _BoilerPlateApplicationState createState() => _BoilerPlateApplicationState();
+}
+
+class _BoilerPlateApplicationState extends State<BoilerPlateApplication> {
+  @override
+  Widget build(BuildContext context) {
+    initSizeService(context);
+
+    return Scaffold(
+      body: PageView(
+        controller: Provider.of<NavBarProvider>(context).navigationController,
+        children: <Widget>[HomePage(), SettingsPage()],
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: NavBar(),
     );
   }
 }
