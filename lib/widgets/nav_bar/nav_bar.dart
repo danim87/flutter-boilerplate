c5aff470842300dba6c5b6ddb78379a40b5fafb9
Tobias Weiss <tobias.weiss@bytabo.de>
2021-01-10 15:41:47 +0100

    added nav_bar and nav_bar item

---
+++ lib/widgets/nav_bar/nav_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../providers/nav_bar_provider.dart';
+import '../../services/size_service.dart';
+import 'nav_bar_item.dart';
+
+class NavBar extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => NavBarState();
+}
+
+class NavBarState extends State<NavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: Container(
+        height: scaleWidth(80),
+        padding: EdgeInsets.symmetric(
+          vertical: scaleWidth(7),
+          horizontal: scaleWidth(15),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: _buildNavBarItems(),
+        ),
+      ),
+      color: Colors.white,
+    );
+  }
+
+  List<NavBarItem> _buildNavBarItems() {
+    return [
+      NavBarItem(
+        index: 0,
+        selectedIndex: Provider.of<TabBarModel>(context).selectedTabIndex,
+        onTap: Provider.of<TabBarModel>(context).selectTab,
+        text: 'Home'
+      ),
+      NavBarItem(
+        index: 1,
+        selectedIndex: Provider.of<TabBarModel>(context).selectedTabIndex,
+        onTap: Provider.of<TabBarModel>(context).selectTab,
+        text: 'Settings'
+      )
+    ];
+  }
+}
