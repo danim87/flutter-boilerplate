a770272ab5413321706d09f5919c3d3033251cab
Tobias Weiss <tobias.weiss@bytabo.de>
2021-03-05 20:01:37 +0100

    removed obsolete changes from home.dart

--- lib/widgets/home/home.dart
+++ lib/widgets/home/home.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../providers/app_provider.dart';
-import '../../providers/data_provider.dart';
-import '../../services/mock_data_service.dart';
 import '../../services/size_service.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,10 +14,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final postService = MockDataService();
-
   @override
   Widget build(BuildContext context) {
+    initSizeService(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
@@ -28,31 +26,12 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Consumer<DataProvider>(
-              builder: (_, dataProvider, __) {
-                if (dataProvider.state == NotifierState.initial) {
-                  return Text('Press the button 👇');
-                } else if (dataProvider.state == NotifierState.loading) {
-                  return CircularProgressIndicator();
-                } else {
-                  return dataProvider.data.fold(
-                    (error) => Text(error.toString()),
-                    (data) => Text(data.toString()),
-                  );
-                }
-              },
+            Text(
+              'button clicks:',
             ),
             Text(
               '${Provider.of<AppProvider>(context).counter}',
               style: Theme.of(context).textTheme.headline4,
-            ),
-            SizedBox(height: scaleHeight(30)),
-            RaisedButton(
-              onPressed: () =>
-                  Provider.of<DataProvider>(context, listen: false).getData(),
-              child: Text(
-                'Get Data',
-              ),
             ),
             SizedBox(height: scaleHeight(30)),
             RaisedButton(
