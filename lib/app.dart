e33ea26fb422151229db55d21be3ed17b0bb683e
Tobias Weiss <tobias.weiss@bytabo.de>
2021-03-05 20:05:45 +0100

    removed inputs in app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -2,12 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'providers/app_provider.dart';
-import 'providers/nav_bar_provider.dart';
+import 'providers/bottom_nav_bar_provider.dart';
 import 'router.dart';
-import 'services/size_service.dart';
-import 'widgets/bottom_nav_bar/bottom_nav_bar.dart';
-import 'widgets/home/home.dart';
-import 'widgets/settings/settings.dart';
 
 class App extends StatelessWidget {
   @override
@@ -15,7 +11,7 @@ class App extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => AppProvider()),
-        ChangeNotifierProvider(create: (context) => NavBarProvider()),
+        ChangeNotifierProvider(create: (context) => BottomNavBarProvider()),
       ],
       child: MaterialApp(
         title: 'Flutter Boilerplate Starter',
